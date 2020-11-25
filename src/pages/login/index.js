52d4176f0cd1f965c3218a6c7639d39fdd0c295d
yeyuhang <yeyuhang@appicplay.com>
2020-11-25 15:20:42 +0800

    update

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,20 +1,46 @@
 /*
  * @Author: yeyuhang
  * @Date: 2020-11-25 12:00:42
- * @LastEditTime: 2020-11-25 12:37:31
+ * @LastEditTime: 2020-11-25 15:20:03
  * @LastEditors: yeyuhang
  * @Descripttion: 头部注释
  */
 import './index.scss'
 function Login() {
+    let timer = new Date().toLocaleTimeString()
     return (
         <div className="App">
+            {/* bg */}
             <div className="container">
                 <div className="window">
                     <div className="moon"></div>
                 </div>
                 <div className="floor"></div>
                 <div className="door"></div>
+            </div>
+            {/* 时间 */}
+            <div class="g-container">
+                <div class="g-number">{timer}</div>
+                <div class="g-contrast">
+                    <div class="g-circle"></div>
+                    <ul class="g-bubbles">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
