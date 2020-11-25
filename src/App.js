03655948d266e876268c5ae84280cac896aaea7a
yeyuhang <yeyuhang@appicplay.com>
2020-11-25 11:28:36 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+/*
+ * @Author: yeyuhang
+ * @Date: 2020-11-25 11:14:13
+ * @LastEditTime: 2020-11-25 11:27:49
+ * @LastEditors: yeyuhang
+ * @Descripttion: 头部注释
+ */
+// import logo from './logo.svg';
 import './App.css';
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>antd version: {version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
       </header>
     </div>
   );
